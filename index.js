d221d92f277f4c331f6d9f36dd64e446764e20a2
Ridwan <myworld.shoyab@gmail.com>
2021-04-14 00:48:08 +0600

    trying

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const port = 5000 || process.env.PORT ;
 const MongoClient = require("mongodb").MongoClient;
 require('dotenv').config()
 const ObjectId = require("mongodb").ObjectId;
@@ -114,7 +113,8 @@ app.get("/", (req, res) => {
 
 
 
-
+  const port = process.env.PORT || 5000 ;
+  
   app.listen(port, () => {
     console.log(`Node is ready`);
   });
